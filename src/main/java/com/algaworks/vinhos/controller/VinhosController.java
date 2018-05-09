9ccf4a1368138ec2021b468ad83a7590b59f088d
andremendoncaolv <andremendoncaolv@gmail.com>
2018-05-08 22:58:34 -0300

    alteracao lista edicao

--- src/main/java/com/algaworks/vinhos/controller/VinhosController.java
+++ src/main/java/com/algaworks/vinhos/controller/VinhosController.java
@@ -1,10 +1,13 @@
 package com.algaworks.vinhos.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.algaworks.vinhos.model.TipoVinho;
 import com.algaworks.vinhos.model.Vinho;
 import com.algaworks.vinhos.repository.Vinhos;
+
+import groovy.lang.Delegate;
 
 @Controller
 @RequestMapping("/vinhos")
@@ -52,8 +57,21 @@ public class VinhosController {
 	}
 	
 	@GetMapping
-	public String listarVinhos() {
-		return "vinhos/lista-vinhos";
+	public ModelAndView listarVinhos() {
+		ModelAndView modelAndView =  new ModelAndView("vinhos/lista-vinhos");
+		modelAndView.addObject("vinhos", vinhos.findAll());
+		
+		return modelAndView;
+	}
+	
+	@DeleteMapping("/{id}")
+	public String remover(@PathVariable Long id, RedirectAttributes attributes) {
+		vinhos.delete(id);
+		
+		attributes.addFlashAttribute("mensagem", "Vinho removido com sucesso!");
+		
+		return "redirect:/vinhos";
+		
 	}
 
 }
