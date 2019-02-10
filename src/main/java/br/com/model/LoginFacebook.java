e9c47b220ef16c35988c45583a5e57d292bb6ead
unknown <rodrigoopor@hotmail.com>
2019-02-10 09:05:17 -0300

    Adicionando dependencia da JSONObject para manipulação de objetos json, Controller responsavel de lidar com as chamadas a API graph do facebook e model respectivo ao controller de seu uso. Login com Facebook ainda não terminado.

---
+++ src/main/java/br/com/model/LoginFacebook.java
@@ -0,0 +1,66 @@
+package br.com.model;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class LoginFacebook {
+	
+	private static final String client_secret = "";
+	private static final String client_id = "2105340609784344";
+	private static final String redirect_uri = "https://localgost:8080/petlove2/loginfdresponse";
+	
+	public void obterUsuarioFacebook(String code) throws MalformedURLException, IOException {
+		
+		String retorno = readURL(new URL(this.getAuthURL(code)));
+		
+		String accessToken = null;
+		@SuppressWarnings("unused")
+		Integer expires = null;
+		String[] pairs = retorno.split("&");
+		for(String pair : pairs) {
+			String[] kv = pair.split("=");
+			if(kv.length != 2) {
+				throw new RuntimeException("Resposta auth inesperada.");
+			}
+			else {
+				if(kv[0].equals("access_token")) {
+					accessToken = kv[1];
+				}
+				if(kv[0].equals("expires")) {
+					expires = Integer.valueOf(kv[1]);
+				}
+			}
+		}
+		
+		JSONObject resp = new JSONObject(readURL(new URL("https://graph.facebook.com/me?access_token=" + accessToken)));
+		
+		UsuarioFacebook usuarioFacebook = new UsuarioFacebook(resp);
+		System.out.println(usuarioFacebook.toString());
+	}
+	
+	private String readURL(URL url) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		InputStream is = url.openStream();
+		int r;
+		while ((r = is.read()) != -1) {
+			baos.write(r);
+		}
+		return new String(baos.toByteArray());
+	}
+	
+	public String getLoginRedirectURL() {
+		return "https://graph.facebook.com/oauth/authorize?client_id="
+				+ client_id + "&display=page&redirect_uri=" + redirect_uri
+				+ "&scope=email,publish_actions";
+	}
+	
+	public String getAuthURL(String authCode) {
+		return "https://graph.facebook.com/oauth/access_token?client_id="
+				+ client_id + "&redirect_uri=" + redirect_uri
+				+ "&client_secret=" + client_secret + "&code=" + authCode;
+	}
+
+}
